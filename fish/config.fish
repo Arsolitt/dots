a6a7c791146e929bd2b90b37602551f62fffbdf6
Arsolitt <arsolitt@gmail.com>
2024-09-22 23:05:59 +0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 # if status is-interactive
     # Commands to run in interactive sessions can go here
 # end
-# if status is-login
-#     exec Hyprland
-# end
+if status is-login
+    exec Hyprland
+end
 # if status is-login
 #     if test -z "$DISPLAY" -a "$(tty)" = /dev/tty1
 #         exec startx -- -keeptty
