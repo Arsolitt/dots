1eeb86d4ec4fc8d18cbde6e9bd49748d61186757
Arsolitt <arsolitt@gmail.com>
2024-09-22 14:29:05 +0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,16 @@
-if status is-interactive
+# if status is-interactive
     # Commands to run in interactive sessions can go here
-end
-if status is-login
-    exec Hyprland
-end
+# end
+# if status is-login
+#     exec Hyprland
+# end
+# if status is-login
+#     if test -z "$DISPLAY" -a "$(tty)" = /dev/tty1
+#         exec startx -- -keeptty
+#     end
+# end
+
 set -gx EDITOR nano
 
+zoxide init fish | source
 zoxide init fish | source
