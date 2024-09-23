394749d841fb543de86029b7fe8c907661b1e8ad
Arsolitt <arsolitt@gmail.com>
2024-09-23 20:30:52 +0300

    update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -sf $PWD/dunst $HOME/.config/dunst
 ln -sf $PWD/fish $HOME/.config/fish
 ln -sf $PWD/gtk-2.0 $HOME/.config/gtk-2.0
 ln -sf $PWD/gtk-3.0 $HOME/.config/gtk-3.0
-ln -sf $PWD/gtk-4.0 $HOME/.config/gtk-4.0
+# ln -sf $PWD/gtk-4.0 $HOME/.config/gtk-4.0
 ln -sf $PWD/hypr $HOME/.config/hypr
 ln -sf $PWD/kitty $HOME/.config/kitty
 ln -sf $PWD/nwg-look $HOME/.config/nwg-look
@@ -15,4 +15,5 @@ ln -sf $PWD/swaylock $HOME/.config/swaylock
 ln -sf $PWD/waybar $HOME/.config/waybar
 ln -sf $PWD/wlogout $HOME/.config/wlogout
 ln -sf $PWD/wofi $HOME/.config/wofi
+ln -sf $PWD/foot $HOME/.config/foot
 ln -sf $PWD/electron-flags.conf $HOME/.config/electron-flags.conf
