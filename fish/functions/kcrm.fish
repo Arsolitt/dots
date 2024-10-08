a0bac4a8438d2e6b3e1e4ba052c001247091b22f
Arsolitt <arsolitt@gmail.com>
2024-10-08 20:39:44 +0300

    update

---
+++ fish/functions/kcrm.fish
@@ -0,0 +1,4 @@
+function kcrm --wraps='k config use-context arsolitt@rmedia' --description 'alias kcrm k config use-context arsolitt@rmedia'
+  k config use-context arsolitt@rmedia $argv
+        
+end
