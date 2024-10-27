30c4a818d2bb39671997b7e9712a2ab0c47554af
Arsolitt <arsolitt@gmail.com>
2024-10-27 15:19:54 +0300

    update

--- fish/functions/gachi.fish
+++ fish/functions/gachi.fish
@@ -1,9 +1,9 @@
 function gachi
-git add .
-if test $argv[1]
-git commit -m "$argv[1]" --no-verify
-else
-git commit -m "update" --no-verify
-end
-git push origin HEAD
+  git add .
+  if test $argv[1]
+    git commit -m "$argv[1]"
+  else
+    git commit -m "update"
+  end
+  git push origin HEAD
 end
