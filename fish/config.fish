668968a2bc1894a91b333b4c82425430358c19f0
Arsolitt <arsolitt@gmail.com>
2025-03-03 22:01:30 +0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -14,3 +14,6 @@ set -gx EDITOR nano
 
 zoxide init fish | source
 set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
--- fish/functions/kch.fish
+++ fish/functions/kch.fish
@@ -1,4 +1,4 @@
-function kch --wraps='k config use-context arsolitt@homelab' --description 'alias kch k config use-context arsolitt@homelab'
-  k config use-context arsolitt@homelab $argv
+function kch --wraps='k config use-context arsolitt@homelab' --wraps='k config use-context arsolitt@home' --description 'alias kch k config use-context arsolitt@home'
+  k config use-context arsolitt@home $argv
         
 end
