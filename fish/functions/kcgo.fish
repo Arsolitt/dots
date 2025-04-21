153067ec236c442c347a845e295c3f32ab3a46fb
Arsolitt <arsolitt@gmail.com>
2025-04-21 21:14:33 +0300

    WIP

---
+++ fish/functions/kcgo.fish
@@ -0,0 +1,4 @@
+function kcgo --wraps='k config use-context arsolitt@gateway_old' --description 'alias kcgo k config use-context arsolitt@gateway_old'
+  k config use-context arsolitt@gateway_old $argv
+        
+end
