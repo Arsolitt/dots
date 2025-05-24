80d2a37ea0026f2754f3d3a52ecf112fef5773d5
Arsolitt <arsolitt@gmail.com>
2025-05-24 17:50:13 +0300

    WIP

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ if status is-login
 end
 
 set -gx EDITOR nano
+set -gx GPG_TTY (tty)
 
 zoxide init fish | source
 set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
