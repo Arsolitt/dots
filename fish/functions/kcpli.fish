e12e64a5aa8b2cbb8c54f48b14f2f223743a0820
Arsolitt <arsolitt@gmail.com>
2025-07-16 12:21:34 +0300

    WIP

---
+++ fish/functions/kcpli.fish
@@ -0,0 +1,4 @@
+function kcpli --wraps='k config use-context arsolitt@psclp-infra' --description 'alias kcpli k config use-context arsolitt@psclp-infra'
+  k config use-context arsolitt@psclp-infra $argv
+        
+end
