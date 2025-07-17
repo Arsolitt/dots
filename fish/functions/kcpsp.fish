ef18c21d343866d7485769dd14bd684e3b9dbd88
Arsolitt <arsolitt@gmail.com>
2025-07-17 15:09:01 +0300

    WIP

---
+++ fish/functions/kcpsp.fish
@@ -0,0 +1,4 @@
+function kcpsp --wraps='k config use-context arsolitt@psc-prod' --description 'alias kcpsp k config use-context arsolitt@psc-prod'
+  k config use-context arsolitt@psc-prod $argv
+        
+end
