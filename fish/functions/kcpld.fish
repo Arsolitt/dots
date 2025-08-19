9060c956aab9a3e98158068026add534db88fc18
Arsolitt <arsolitt@gmail.com>
2025-08-19 17:59:45 +0300

    WIP

---
+++ fish/functions/kcpld.fish
@@ -0,0 +1,4 @@
+function kcpld --wraps='k config use-context arsolitt@psclp-dev' --description 'alias kcpld k config use-context arsolitt@psclp-dev'
+  k config use-context arsolitt@psclp-dev $argv
+        
+end
