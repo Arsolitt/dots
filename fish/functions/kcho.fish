ea4bfcc3b46691cd112325bd1545d899c33e4567
Arsolitt <arsolitt@gmail.com>
2025-09-19 14:53:55 +0300

    WIP

---
+++ fish/functions/kcho.fish
@@ -0,0 +1,4 @@
+function kcho --wraps='k config use-context arsolitt@home_old' --description 'alias kcho k config use-context arsolitt@home_old'
+  k config use-context arsolitt@home_old $argv
+        
+end
--- fish/functions/ksh.fish
+++
@@ -1,4 +0,0 @@
-function ksh --wraps='kitten ssh' --description 'alias ksh kitten ssh'
-  kitten ssh $argv
-        
-end
