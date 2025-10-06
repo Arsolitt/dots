f924e03a98537f07be1dda4b2f2c88f75f5625d3
Arsolitt <arsolitt@gmail.com>
2025-10-06 16:42:11 +0300

    WIP

--- fish/functions/gitcloc.fish
+++ fish/functions/gitcloc.fish
@@ -1,4 +1,4 @@
-function gitcloc --wraps='cloc 8c0b9ee' --description 'alias gitcloc cloc 8c0b9ee'
-  cloc 8c0b9ee $argv
+function gitcloc --wraps='cloc 8c0b9ee' --wraps='cloc $(git log -1 --pretty=format:%h)' --description 'alias gitcloc cloc $(git log -1 --pretty=format:%h)'
+  cloc $(git log -1 --pretty=format:%h) $argv
         
 end
