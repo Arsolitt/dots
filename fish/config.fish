378cd08c11361b75cac59e643b132ed89d6840f9
Arsolitt <arsolitt@gmail.com>
2025-11-17 20:13:31 +0300

    WIP

--- fish/config.fish
+++ fish/config.fish
@@ -7,12 +7,14 @@ end
 set -gx GPG_TTY (tty)
 set -gx EDITOR nano
 
-set --export PATH $PATH /home/arsolitt/vk-cloud-solutions/bin
+set --export PATH $PATH ~/vk-cloud-solutions/bin
+
+set --export PATH $PATH ~/go/bin
 
 zoxide init fish | source
 set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-set --export PATH /home/arsolitt/.cargo/bin $PATH
+set --export PATH ~/.cargo/bin $PATH
 source ~/.config/fish/themes/kanagawa.fish
