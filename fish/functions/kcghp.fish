cd1f8d9dbb357e2f6c8b2be6648982ab5e1495b8
Arsolitt <arsolitt@gmail.com>
2026-02-10 13:42:54 +0300

    WIP

---
+++ fish/functions/kcghp.fish
@@ -0,0 +1,3 @@
+function kcghp --wraps='k config use-context arsolitt@gh-prod' --description 'alias kcghp k config use-context arsolitt@gh-prod'
+    k config use-context arsolitt@gh-prod $argv
+end
