7b411dc0ecb007b9d34e857cd9d4a017a6c40f08
Arsolitt <arsolitt@gmail.com>
2026-03-07 11:32:02 +0300

    WIP

---
+++ fish/functions/kclpp.fish
@@ -0,0 +1,3 @@
+function kclpp --wraps='k config use-context arsolitt@lp-prod' --description 'alias kclpp k config use-context arsolitt@lp-prod'
+    k config use-context arsolitt@lp-prod $argv
+end
