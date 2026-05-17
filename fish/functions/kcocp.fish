2c9f67f287a20f45ecba1234a7035340fafd5d70
Arsolitt <arsolitt@gmail.com>
2026-05-17 13:22:00 +0300

    feat(fish): add kcocp alias for ock-prod context
    Shortcut for switching the active kubectl context to arsolitt@ock-prod.
    
    Signed-off-by: Arsolitt <arsolitt@gmail.com>

---
+++ fish/functions/kcocp.fish
@@ -0,0 +1,3 @@
+function kcocp --wraps='k config use-context arsolitt@ock-prod' --description 'alias kcocp k config use-context arsolitt@ock-prod'
+    k config use-context arsolitt@ock-prod $argv
+end
